2fed03e3446642966f89187800a05fe283a73ebd
じぃ <161315295+Ji-113@users.noreply.github.com>
2024-05-05 18:40:24 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+// Cache name
+const CACHE_NAME = 'pwa-sample-caches-v1';
+// Cache targets
+const urlsToCache = [
+  './',
+  './index.html',
+  './conan-charactor/index.html',
+  './pages/b.html',
+  './pages/c.html',
+  './css/style.css',
+  './images/コナン最高.jpg',
+  './images/江戸川コナン.png',
+  './images/favicon.ico',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
+});
